d370344e1dc765d6506540f9c5d507d3eb8d04fd
Christian Heimes <christian@python.org>
2013-02-10 17:24:09 +0100

    add xml category

--- setup.py
+++ setup.py
@@ -148,5 +148,6 @@ setup(
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
         # "Programming Language :: Python :: 3.4",
+        "Topic :: Text Processing :: Markup :: XML",
     ],
 )
